6ab238acc7a76dfe40b133532989101a47a9498e
Ali Nouri <dynamic11@gmail.com>
2021-12-01 21:07:03 -0500

    [new] add react bootstrap buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,47 @@
 import React from "react";
+import { default as ButtonRB } from "react-bootstrap/Button";
 import "./Button.css";
 // import "../../style.css";
 
+type varientTypes =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "warning"
+  | "danger"
+  | "light"
+  | "link"
+  | "info"
+  | "dark"
+  | "outline-primary"
+  | "outline-secondary"
+  | "outline-success"
+  | "outline-warning"
+  | "outline-danger"
+  | "outline-info"
+  | "outline-light"
+  | "outline-dark";
+
+type sizingTypes = "lg" | "sm";
+
+type typeTypes = "submit" | "button" | "reset";
+type asTypes = "input";
+
 export interface ButtonProps {
-  label: string;
+  children?: React.ReactNode;
+  variant?: varientTypes;
+  size?: sizingTypes;
+  className?: string;
+  active?: boolean;
+  disabled?: boolean;
+  href?: string;
+  type?: typeTypes;
+  value?: string;
+  as?: asTypes;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button className="btn">{props.label}</button>;
+const Button = ({ children, ...rest }: ButtonProps) => {
+  return <ButtonRB {...rest}>{children}</ButtonRB>;
 };
 
 export default Button;
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -5,4 +5,57 @@ import { Button } from "../components";
 
 const stories = storiesOf("App Test", module);
 
-stories.add("App", () => <Button label="Hello World" />);
+stories.add("App", () => {
+  return (
+    <>
+      <h1>Buttons</h1>
+      <h2>Regular Buttons</h2>
+      <Button variant="primary">Default</Button>{" "}
+      <Button variant="secondary">Secondary</Button>{" "}
+      <Button variant="danger">Danger</Button>{" "}
+      <Button variant="light">Light</Button>{" "}
+      <Button variant="link">Link</Button>{" "}
+      <Button variant="success">Success</Button>{" "}
+      <Button variant="warning">Warning</Button>{" "}
+      <Button variant="info">Info</Button> <Button variant="dark">Dark</Button>{" "}
+      <h2>Outline Buttons (not supported)</h2>
+      <Button variant="outline-primary">Default</Button>{" "}
+      <Button variant="outline-secondary">Secondary</Button>{" "}
+      <Button variant="outline-danger">Danger</Button>{" "}
+      <Button variant="outline-light">Light</Button>{" "}
+      <Button variant="outline-success">Success</Button>{" "}
+      <Button variant="outline-warning">Warning</Button>{" "}
+      <Button variant="outline-info">Info</Button>{" "}
+      <Button variant="outline-dark">Dark</Button>
+      <h2>Sizes</h2>
+      <Button variant="outline-primary" size="lg">
+        Large
+      </Button>{" "}
+      <Button variant="outline-secondary" size="sm">
+        Small
+      </Button>
+      <h2>Active state</h2>
+      <Button variant="primary" size="lg" active>
+        Primary button
+      </Button>{" "}
+      <Button variant="secondary" size="lg" active>
+        Button
+      </Button>
+      <h2>Disabled state</h2>
+      <Button variant="primary" size="lg" disabled>
+        Primary button
+      </Button>{" "}
+      <Button variant="secondary" size="lg" disabled>
+        Button
+      </Button>
+      <h2>Button tags</h2>
+      <Button variant="primary" href="#">
+        Link
+      </Button>{" "}
+      <Button type="submit">Button</Button>{" "}
+      <Button as="input" type="button" value="Input" />{" "}
+      <Button as="input" type="submit" value="Submit" />{" "}
+      <Button as="input" type="reset" value="Reset" />
+    </>
+  );
+});
