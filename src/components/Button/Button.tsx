fa3650ee6e8fecc60ca8558e427d4d760ef05921
Ali Nouri <dynamic11@gmail.com>
2021-12-01 19:07:27 -0500

    import gcweb theme css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Button.css";
+// import "../../style.css";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className="btn">{props.label}</button>;
 };
 
 export default Button;
