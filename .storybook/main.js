1fff0c7f57f731f38fabc928843f6ab1997d8669
Ali Nouri <dynamic11@gmail.com>
2021-12-03 11:22:27 -0500

    [new] add a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+};
