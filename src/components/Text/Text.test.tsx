0ced773468725b1c706a98b81829a032d2d8ad72
Ali Nouri <dynamic11@gmail.com>
2021-12-04 20:41:17 -0500

    [fix] resolve test issues for Title and Text components

---
+++ src/components/Text/Text.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Text from "./Text";
+
+describe("Text", () => {
+  test("renders sample text", () => {
+    render(<Text>Hello world!</Text>);
+  });
+});
--- src/components/Title/Title.test.tsx
+++ src/components/Title/Title.test.tsx
@@ -5,21 +5,21 @@ import Title from "./Title";
 
 describe("Title", () => {
   test("renders the level 1 Title component", () => {
-    render(<Title level={1}>Hello world!</Title>);
+    render(<Title level="h1">Hello world!</Title>);
   });
   test("renders the level 2 Title component", () => {
-    render(<Title level={2}>Hello world!</Title>);
+    render(<Title level="h2">Hello world!</Title>);
   });
   test("renders the level 3 Title component", () => {
-    render(<Title level={3}>Hello world!</Title>);
+    render(<Title level="h3">Hello world!</Title>);
   });
   test("renders the level 4 Title component", () => {
-    render(<Title level={4}>Hello world!</Title>);
+    render(<Title level="h4">Hello world!</Title>);
   });
   test("renders the level 5 Title component", () => {
-    render(<Title level={5}>Hello world!</Title>);
+    render(<Title level="h5">Hello world!</Title>);
   });
   test("renders the level 6 Title component", () => {
-    render(<Title level={6}>Hello world!</Title>);
+    render(<Title level="h6">Hello world!</Title>);
   });
 });
