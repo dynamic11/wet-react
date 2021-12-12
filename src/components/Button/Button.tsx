b6902bd1f7a1ae497e04e2d9a1a27738d6e508d7
Ali Nouri <dynamic11@gmail.com>
2021-12-12 01:09:30 -0500

    [new] add eslint and prettier

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,83 +1,83 @@
-import React from "react";
-import { default as ButtonRB } from "react-bootstrap/Button";
-import "../../style.css";
+import React from 'react'
+import ButtonRB from 'react-bootstrap/Button'
+import '../../style.css'
 
 /** Types */
 type variantType =
-  | "primary"
-  | "secondary"
-  | "success"
-  | "warning"
-  | "danger"
-  | "light"
-  | "link"
-  | "info"
-  | "dark"
-  | undefined;
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'warning'
+    | 'danger'
+    | 'light'
+    | 'link'
+    | 'info'
+    | 'dark'
+    | undefined
 
-type sizingType = "lg" | "sm" | "xs" | undefined;
+type sizingType = 'lg' | 'sm' | 'xs' | undefined
 
-type typeType = "submit" | "button" | "reset" | undefined;
-type asType = "input";
+type typeType = 'submit' | 'button' | 'reset' | undefined
+type asType = 'input'
 
 export interface ButtonProps {
-  /** Content of button */
-  children?: React.ReactNode;
-  /** The styling variant that you would like to use */
-  variant?: variantType;
-  /** The custom 'non-default' button size that you would like */
-  size?: sizingType;
-  /** Is button in Active state */
-  active?: boolean;
-  /** Option to fit button width to its parent width */
-  block?: boolean;
-  /** Is button in Disabled state */
-  disabled?: boolean;
-  /** Link for button. It will render as `<a>` but with button styling */
-  href?: string;
-  /** Type of button */
-  type?: typeType;
-  /** Value associated to the button */
-  value?: string;
-  as?: asType;
-  /** Additional custom classNames */
-  className?: string;
-  /** Onclick action */
-  onClick?: React.MouseEventHandler;
+    /** Content of button */
+    children?: React.ReactNode
+    /** The styling variant that you would like to use */
+    variant?: variantType
+    /** The custom 'non-default' button size that you would like */
+    size?: sizingType
+    /** Is button in Active state */
+    active?: boolean
+    /** Option to fit button width to its parent width */
+    block?: boolean
+    /** Is button in Disabled state */
+    disabled?: boolean
+    /** Link for button. It will render as `<a>` but with button styling */
+    href?: string
+    /** Type of button */
+    type?: typeType
+    /** Value associated to the button */
+    value?: string
+    as?: asType
+    /** Additional custom classNames */
+    className?: string
+    /** Onclick action */
+    onClick?: React.MouseEventHandler
 }
 
 const Button = ({
-  children,
-  variant = "primary",
-  active = false,
-  disabled = false,
-  block = false,
-  size,
-  className = "",
-  ...rest
+    children,
+    variant = 'primary',
+    active = false,
+    disabled = false,
+    block = false,
+    size,
+    className = '',
+    ...rest
 }: ButtonProps) => {
-  const sizeClassName =
-    size === "lg"
-      ? "btn-lg"
-      : size === "sm"
-      ? "btn-sm"
-      : size === "xs"
-      ? "btn-xs"
-      : "";
+    const sizeClassName =
+        size === 'lg'
+            ? 'btn-lg'
+            : size === 'sm'
+            ? 'btn-sm'
+            : size === 'xs'
+            ? 'btn-xs'
+            : ''
 
-  const blockClassName = block ? "btn-block" : "";
+    const blockClassName = block ? 'btn-block' : ''
 
-  return (
-    <ButtonRB
-      variant={variant}
-      active={active}
-      disabled={disabled}
-      className={`${sizeClassName} ${className} ${blockClassName}`}
-      {...rest}
-    >
-      {children}
-    </ButtonRB>
-  );
-};
+    return (
+        <ButtonRB
+            variant={variant}
+            active={active}
+            disabled={disabled}
+            className={`${sizeClassName} ${className} ${blockClassName}`}
+            {...rest}
+        >
+            {children}
+        </ButtonRB>
+    )
+}
 
-export default Button;
+export default Button
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
 export { default as Title } from "./Title";
-export { default as Text } from "./Title";
+export { default as Text } from "./Text";
 export { default as Link } from "./Link";
+export { default as Alert } from "./Alert";
