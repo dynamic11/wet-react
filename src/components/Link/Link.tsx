f00f3c75f5953bb25b9bb81c171962b9ec57840a
Ali Nouri <dynamic11@gmail.com>
2021-12-12 02:49:20 -0500

    [new] add 'target' data type for links

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import "../../style.css";
+import React from 'react';
+import '../../style.css';
+
+type targetType = '_blank' | '_self' | '_parent' | '_top' | undefined;
 
 /** Types */
 export interface LinkProps {
@@ -8,14 +10,14 @@ export interface LinkProps {
   /** The target of hyperlink */
   href: string;
   /** Specifies where to display the linked URL */
-  target?: string;
+  target?: targetType;
   /** Specifies the relationship between the current document and the linked document. */
   rel?: string;
   /** Additional custom classNames */
   className?: string;
 }
 
-const Link = ({ children, href = "#", target, rel, ...rest }: LinkProps) => (
+const Link = ({ children, href = '#', target, rel, ...rest }: LinkProps) => (
   <a href={href} target={target} rel={rel} {...rest}>
     {children}
   </a>
