d45d5d89922cc8a94111ec04b69dba910948a437
Ali Nouri <dynamic11@gmail.com>
2021-12-16 01:51:59 -0500

    [New] add a custom theme/logo to storybook

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'WET React Library',
+  brandImage:
+    'https://user-images.githubusercontent.com/11470442/145418747-6b58458d-02f5-499f-ba52-96a4cc181efa.png',
+});
