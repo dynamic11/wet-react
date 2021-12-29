4c425ad9abe6b2f01d86c4485c97787cf131c891
Ali Nouri <dynamic11@gmail.com>
2021-12-29 12:53:33 -0500

    [fix] improve tests for Badge component

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Badge from '@components/Badge';
 
 describe('Button', () => {
-  test('renders the Button component', () => {
-    render(<Badge>Hello world!</Badge>);
+  test('renders the Badge component', () => {
+    const badgeContent = 'Hello world!';
+    render(<Badge>{badgeContent}</Badge>);
+    expect(screen.getByText(badgeContent)).toBeInTheDocument();
+    expect(screen.getByText(badgeContent)).toHaveClass('badge');
+  });
+
+  test('renders the Badge component with badgeLabel', () => {
+    const badgeContent = 'Hello world!';
+    const badgeLabelContent = 'This is hidden text';
+    render(<Badge badgeLabel={badgeLabelContent}>{badgeContent}</Badge>);
+    expect(screen.getByText(badgeContent)).toBeInTheDocument();
+    expect(screen.getByText(badgeLabelContent)).toBeInTheDocument();
+    expect(screen.getByText(badgeLabelContent)).toHaveClass('wb-inv');
+  });
+
+  test('hide Badge component using visibility', () => {
+    const badgeContent = 'Hello world!';
+    const badgeLabelContent = 'This is hidden text';
+    render(
+      <Badge badgeLabel={badgeLabelContent} visible={false}>
+        {badgeContent}
+      </Badge>
+    );
+    expect(screen.queryByText(badgeContent)).not.toBeInTheDocument();
+    expect(screen.queryByText(badgeLabelContent)).not.toBeInTheDocument();
+  });
+
+  test('check for custom className', () => {
+    const badgeContent = 'Hello world!';
+    const className = 'test-class';
+    render(<Badge className={className}>{badgeContent}</Badge>);
+    expect(screen.queryByText(badgeContent)).toHaveClass(className);
   });
 });
