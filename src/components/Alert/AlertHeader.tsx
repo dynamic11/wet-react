b691bb1f6c4345ceb75a6746d6e7f58b10e896a1
Ali Nouri <dynamic11@gmail.com>
2022-01-17 13:10:25 -0500

    extract subcomponents as independent components under alert

---
+++ src/components/Alert/AlertHeader.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Title from '@components/Title';
+
+type levelType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | undefined;
+
+export interface AlertHeaderProps {
+  /** Content of alert heading */
+  children?: React.ReactNode;
+  /** Header Level */
+  level?: levelType;
+  /** Additional custom classNames */
+  className?: string;
+}
+
+const AlertHeader = ({
+  level = 'h2',
+  className = '',
+  children,
+}: AlertHeaderProps) => (
+  <Title level={level} className={className}>
+    {children}
+  </Title>
+);
+
+AlertHeader.displayName = 'Alert.Header';
+
+export default AlertHeader;
--- src/components/Alert/index.ts
+++ src/components/Alert/index.ts
@@ -1,3 +1,18 @@
-import Alert from './Alert';
+import OriginAlert from './Alert';
+import AlertHeader from './AlertHeader';
+import AlertBody from './AlertBody';
+import AlertLink from './AlertLink';
+
+export type AlertProps = typeof OriginAlert & {
+  Header: typeof AlertHeader;
+  Body: typeof AlertBody;
+  Link: typeof AlertLink;
+};
+
+const Alert = OriginAlert as AlertProps;
+
+Alert.Header = AlertHeader;
+Alert.Body = AlertBody;
+Alert.Link = AlertLink;
 
 export default Alert;
