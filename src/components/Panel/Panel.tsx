00ff84e3d681d543f6773497de961e577cb30b31
Ali Nouri <dynamic11@users.noreply.github.com>
2022-01-18 16:01:19 -0500

    [New] implement Panel component (#13)

---
+++ src/components/Panel/Panel.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Title from '@components/Title';
+import '../../style.css';
+
+/** Types */
+type variantType = 'success' | 'warning' | 'danger' | 'info' | undefined;
+type headingLevelType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | undefined;
+
+export interface PanelProps {
+  /** The styling variant that you would like to use */
+  variant?: variantType;
+  /** The content of panel header */
+  headerContent?: React.ReactNode;
+  /** The heading level of panel header */
+  headerLevel?: headingLevelType;
+  /** The content of panel footer */
+  footerContent?: React.ReactNode;
+  /** Show panel */
+  visible?: boolean;
+  /** Content of panel */
+  children?: React.ReactNode;
+  /** Additional custom classNames */
+  className?: string;
+}
+
+const Panel = ({
+  variant = 'info',
+  visible = true,
+  className = '',
+  headerContent,
+  headerLevel = 'h2',
+  footerContent,
+  children,
+  ...rest
+}: PanelProps) => {
+  const panelClassName =
+    variant === 'success'
+      ? 'panel-success'
+      : variant === 'warning'
+      ? 'panel-warning'
+      : variant === 'danger'
+      ? 'panel-danger'
+      : 'panel-info';
+
+  return visible ? (
+    <div className={`panel ${panelClassName} ${className}`} {...rest}>
+      {headerContent && (
+        <header className="panel-heading">
+          <Title level={headerLevel} className="panel-title">
+            {headerContent}
+          </Title>
+        </header>
+      )}
+      <div className="panel-body">{children}</div>
+      {footerContent && (
+        <footer className="panel-footer">{footerContent}</footer>
+      )}
+    </div>
+  ) : null;
+};
+
+Panel.displayName = 'Panel';
+
+export default Panel;
