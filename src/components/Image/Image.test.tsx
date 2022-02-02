737c0745f75b9070816e09bd2ca765753fdf9bf8
Mayowa Awosiyan <mayowaa4@gmail.com>
2022-02-02 11:38:56 -0500

    Updated Image tests

--- src/components/Image/Image.test.tsx
+++ src/components/Image/Image.test.tsx
@@ -6,14 +6,19 @@ import ImageWET from '@components/Image';
 
 describe('Image Tests', () => {
   test('Test image component', () => {
-    render(<ImageWET rounded />);
-    expect(screen.getByRole('img')).toHaveClass('rounded');
-  });
-  test('Test image source', () => {
     render(
       <ImageWET
         src="https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA="
         roundedCircle
+      />
+    );
+    expect(screen.getByRole('img')).toHaveClass('img-circle');
+  });
+  test('Test image source and alt', () => {
+    render(
+      <ImageWET
+        src="https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA="
+        thumbnail
         alt="Cool Cat Photo"
       />
     );
@@ -22,5 +27,17 @@ describe('Image Tests', () => {
       'https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA='
     );
     expect(screen.getByRole('img')).toHaveAttribute('alt', 'Cool Cat Photo');
+    expect(screen.getByRole('img')).toHaveClass('img-thumbnail');
+  });
+  test('Test Image styles by class', () => {
+    render(
+      <ImageWET
+        src="https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA="
+        thumbnail
+        rounded
+      />
+    );
+    expect(screen.getByRole('img')).toHaveClass('img-thumbnail');
+    expect(screen.getByRole('img')).toHaveClass('img-rounded');
   });
 });
