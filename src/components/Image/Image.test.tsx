388c55e34c14da5c3178fd30aeefecbf714aa367
Mayowa Awosiyan <mayowaa4@gmail.com>
2022-02-02 19:03:38 -0500

    Pull Request Fixes

--- src/components/Image/Image.test.tsx
+++ src/components/Image/Image.test.tsx
@@ -5,18 +5,29 @@ import '@testing-library/jest-dom';
 import ImageWET from '@components/Image';
 
 describe('Image Tests', () => {
-  test('Test image component', () => {
+  test('Test image component classes', () => {
     render(
       <ImageWET
         src="https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA="
         roundedCircle
+        thumbnail
+        rounded
+        responsive
+        fullWidth
+        media
+        thumbnailTile
       />
     );
     expect(screen.getByRole('img')).toHaveClass('img-circle');
+    expect(screen.getByRole('img')).toHaveClass('img-thumbnail');
+    expect(screen.getByRole('img')).toHaveClass('img-rounded');
+    expect(screen.getByRole('img')).toHaveClass('full-width');
+    expect(screen.getByRole('img')).toHaveClass('thumbnail');
+    expect(screen.getByRole('img')).toHaveClass('media-object');
+    expect(screen.getByRole('img')).toHaveClass('img-responsive');
   });
   test('Test image defaults', () => {
     render(<ImageWET rounded />);
-    expect(screen.getByRole('img')).toHaveClass('img-rounded');
     expect(screen.getByRole('img')).toHaveAttribute('src', '');
     expect(screen.getByRole('img')).toHaveAttribute('alt', '');
   });
@@ -33,17 +44,5 @@ describe('Image Tests', () => {
       'https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA='
     );
     expect(screen.getByRole('img')).toHaveAttribute('alt', 'Cool Cat Photo');
-    expect(screen.getByRole('img')).toHaveClass('img-thumbnail');
-  });
-  test('Test Image styles by class', () => {
-    render(
-      <ImageWET
-        src="https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA="
-        thumbnail
-        rounded
-      />
-    );
-    expect(screen.getByRole('img')).toHaveClass('img-thumbnail');
-    expect(screen.getByRole('img')).toHaveClass('img-rounded');
   });
 });
--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -9,6 +9,14 @@ export interface ImageWETProps {
   roundedCircle?: boolean;
   /** Sets image shape as thumbnail. */
   thumbnail?: boolean;
+  /** Use to make larger images scale properly, and never exceed the width of the parent container */
+  responsive?: boolean;
+  /** Use to stretch a smaller image to the width of the container. */
+  fullWidth?: boolean;
+  /** Use to add any kind of content like headings, paragraphs, or buttons into a thumbnail, to create a thumbnail tile effect */
+  thumbnailTile?: boolean;
+  /** Use to layer text and images for media content like blog comments, tweets, and so forth. */
+  media?: boolean;
   /** Sets the source of the image */
   src?: string;
   /** Sets the alternate text for the image */
@@ -19,6 +27,10 @@ const ImageWET = ({
   rounded = false,
   roundedCircle = false,
   thumbnail = false,
+  responsive = false,
+  fullWidth = false,
+  thumbnailTile = false,
+  media = false,
   src = '',
   alt = '',
   ...rest
@@ -26,16 +38,13 @@ const ImageWET = ({
   const styles = rounded === true ? 'img-rounded' : '';
   const styles2 = roundedCircle === true ? 'img-circle' : '';
   const styles3 = thumbnail === true ? 'img-thumbnail' : '';
-  const finalstyle = `${styles} ${styles2} ${styles3}`;
+  const styles4 = responsive === true ? 'img-responsive' : '';
+  const styles5 = fullWidth === true ? 'full-width' : '';
+  const styles6 = thumbnailTile === true ? 'thumbnail' : '';
+  const styles7 = media === true ? 'media-object' : '';
+  const finalstyle = `${styles} ${styles2} ${styles3} ${styles4} ${styles5} ${styles6} ${styles7}`;
 
-  return (
-    <Image
-      src={src}
-      alt={alt}
-      className={finalstyle.trimStart().trimEnd()}
-      {...rest}
-    />
-  );
+  return <Image src={src} alt={alt} className={finalstyle} {...rest} />;
 };
 
 export default ImageWET;
