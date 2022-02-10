d361d5c585ec1e194593c101f9de5b2b5ef02b80
Ali Nouri <dynamic11@gmail.com>
2022-02-09 23:12:55 -0500

    add form check

---
+++ src/components/Form/FormCheck.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import FormRB from 'react-bootstrap/Form';
+import '../../style.css';
+
+/** Types */
+type typeType = 'checkbox' | 'radio' | 'switch' | undefined;
+
+export interface FormCheckProps {
+  /** title attribute */
+  title?: string;
+  /** label attribute */
+  label?: React.ReactNode;
+  /** The HTML input type, which is only relevant if as is 'input' (the default). */
+  type?: typeType;
+  /** Make the control disabled. */
+  isDisabled?: boolean;
+  /** Groups controls horizontally with other <Form.Checks>. */
+  isInline?: boolean;
+  /** Add "aria-invalid="true" to input */
+  isInvalid?: boolean;
+  /** Add "aria-required="true" to input */
+  isRequired?: boolean;
+  /** Uses controlId from <FormGroup> if not explicitly specified. */
+  id?: string;
+  /** Additional custom classNames */
+  className?: string;
+}
+
+const FormCheck = ({
+  title = '',
+  label,
+  type = 'checkbox',
+  isDisabled = false,
+  isInline = false,
+  isInvalid = false,
+  isRequired = false,
+  id,
+  className = '',
+  ...rest
+}: FormCheckProps) => (
+  <FormRB.Check
+    title={title}
+    label={label}
+    type={type}
+    disabled={isDisabled}
+    inline={isInline}
+    aria-required={isRequired}
+    aria-invalid={isInvalid}
+    id={id}
+    className={`${type} ${className}`}
+    {...rest}
+  />
+);
+
+FormCheck.displayName = 'Form.Check';
+
+export default FormCheck;
