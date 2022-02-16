dbe4cc3fb41775756306dbe3231284669aa0bd13
Mayowa Awosiyan <mayowaa4@gmail.com>
2022-02-16 13:41:26 -0500

    Updated Jumbotron

--- src/components/Jumbotron/Jumbotron.tsx
+++ src/components/Jumbotron/Jumbotron.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import '../../style.css';
 
-export interface JumbotronProps {
+export interface JumbotronProps extends React.HTMLAttributes<HTMLDivElement> {
   /** The content of the jumbotron */
   children?: React.ReactNode;
-  /** Determines if the jumbotron takes up the full width on the container with no rounded corners. Should be */
+  /** Determines if the jumbotron takes up the full width on the container with no rounded corners */
   fluid?: boolean;
 }
 
 const Jumbotron = ({ children, fluid = false, ...rest }: JumbotronProps) => {
   const style = fluid ? 'container-fluid' : '';
-  return <div className={`jumbotron ${style}`}>{children}</div>;
+  return <section className={`jumbotron ${style}`}>{children}</section>;
 };
 
 export default Jumbotron;
