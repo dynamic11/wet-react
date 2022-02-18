8cf3914f952826ac6a4b2afd5a969b2c1404bc6e
Mayowa Awosiyan <mayowaa4@gmail.com>
2022-02-17 21:33:44 -0500

    Added Tests and Stories for Meter

---
+++ src/components/Meter/Meter.test.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Meter from '@components/Meter';
+
+describe('Meter Tests', () => {
+  describe('Meter set value tests', () => {
+    test('Test Meter min value', () => {
+      const target = render(<Meter min={10} max={101} value={37} />);
+      expect(target.container.firstChild).toHaveAttribute('min', '10');
+    });
+    test('Test max value', () => {
+      const target = render(<Meter min={10} max={101} value={37} />);
+      expect(target.container.firstChild).toHaveAttribute('max', '101');
+    });
+    test('Test current value', () => {
+      const target = render(<Meter min={10} max={101} value={37} />);
+      expect(target.container.firstChild).toHaveAttribute('value', '37');
+    });
+    test('Test low value', () => {
+      const target = render(<Meter min={10} max={101} value={37} low={30} />);
+      expect(target.container.firstChild).toHaveAttribute('low', '30');
+    });
+    test('Test high value', () => {
+      const target = render(<Meter min={10} max={101} value={37} high={60} />);
+      expect(target.container.firstChild).toHaveAttribute('high', '60');
+    });
+  });
+  describe('Test for illogical inputs', () => {
+    test('Test low is less than min', () => {
+      const target = render(
+        <Meter min={10} max={101} value={37} low={0} high={60} />
+      );
+      expect(target.container.firstChild).toHaveAttribute('low', '10');
+      expect(target.container.firstChild).toHaveAttribute('min', '10');
+    });
+    test('Test high is greater than max', () => {
+      const target = render(
+        <Meter min={10} max={101} value={37} low={20} high={600} />
+      );
+      expect(target.container.firstChild).toHaveAttribute('high', '101');
+      expect(target.container.firstChild).toHaveAttribute('max', '101');
+    });
+    test('Test min is greater than max', () => {
+      const target = render(
+        <Meter min={105} max={101} value={37} low={0} high={60} />
+      );
+      expect(target.container.firstChild).toHaveAttribute('min', '101');
+      expect(target.container.firstChild).toHaveAttribute('max', '101');
+    });
+    test('Test value is less than min', () => {
+      const target = render(
+        <Meter min={10} max={101} value={0} low={20} high={60} />
+      );
+      expect(target.container.firstChild).toHaveAttribute('value', '10');
+      expect(target.container.firstChild).toHaveAttribute('min', '10');
+    });
+    test('Test value is more than max', () => {
+      const target = render(
+        <Meter min={10} max={101} value={311117} low={0} high={600} />
+      );
+      expect(target.container.firstChild).toHaveAttribute('high', '101');
+      expect(target.container.firstChild).toHaveAttribute('max', '101');
+      expect(target.container.firstChild).toHaveAttribute('value', '101');
+    });
+  });
+  describe('Test default values', () => {
+    test('Test default min', () => {
+      const target = render(<Meter max={100} />);
+      expect(target.container.firstChild).toHaveAttribute('min', '0');
+    });
+    test('Test default max', () => {
+      const target = render(<Meter value={100} />);
+      expect(target.container.firstChild).toHaveAttribute('max', '100');
+    });
+    test('Test default value', () => {
+      const target = render(<Meter max={100} />);
+      expect(target.container.firstChild).toHaveAttribute('value', '0');
+    });
+    test('Test default low', () => {
+      const target = render(<Meter max={100} />);
+      expect(target.container.firstChild).toHaveAttribute('low', '0');
+    });
+    test('Test default high', () => {
+      const target = render(<Meter max={100} />);
+      expect(target.container.firstChild).toHaveAttribute('high', '100');
+    });
+  });
+});
--- src/components/Meter/Meter.tsx
+++ src/components/Meter/Meter.tsx
@@ -9,14 +9,36 @@ export interface MeterProps extends React.HTMLAttributes<HTMLDivElement> {
   max: number;
   /** The lower limit of the meter */
   min: number;
+  /** The point at which a value can be considered low */
+  low?: number;
+  /** The point at which a value can be considered high */
+  high?: number;
 }
 
 const Meter = ({
   max = 100,
   min = 0,
   value = 0,
+  low = 0,
+  high = 100,
   children,
   ...rest
-}: MeterProps) => <meter min={min} max={max} value={value} />;
+}: MeterProps) => {
+  const sterilizedMin = min > max ? max : min;
+  const sterilizedValue =
+    value < sterilizedMin ? sterilizedMin : value > max ? max : value;
+
+  const sterilizedLow = low < min ? min : low;
+  const sterilizedHigh = high > max ? max : high;
+  return (
+    <meter
+      min={sterilizedMin}
+      max={max}
+      value={sterilizedValue}
+      low={sterilizedLow}
+      high={sterilizedHigh}
+    />
+  );
+};
 
 export default Meter;
