94d49ab54c734011ef627a13ca2fef10d3775ff8
Mayowa Awosiyan <61680329+alphaoreo@users.noreply.github.com>
2022-03-11 10:28:19 -0500

    [New] Implement meter component (#70)
    - Added new Meter component
    - Added stories for Meter component
    - Added tests for Meter component

---
+++ src/components/Meter/Meter.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import '../../style.css';
+
+export interface MeterProps extends React.HTMLAttributes<HTMLDivElement> {
+  /** The value of the meter */
+  value: number;
+  /** The upper limit of the meter */
+  max: number;
+  /** The lower limit of the meter */
+  min: number;
+  /** The point at which a value can be considered low */
+  low?: number;
+  /** The point at which a value can be considered high */
+  high?: number;
+}
+
+const Meter = ({
+  max = 100,
+  min = 0,
+  value = 0,
+  low = 0,
+  high = 100,
+  ...rest
+}: MeterProps) => {
+  const sterilizedMin = min > max ? max : min;
+  const sterilizedValue =
+    value < sterilizedMin ? sterilizedMin : value > max ? max : value;
+
+  const sterilizedLow = low < min ? min : low;
+  const sterilizedHigh = high > max ? max : high;
+  return (
+    <meter
+      min={sterilizedMin}
+      max={max}
+      value={sterilizedValue}
+      low={sterilizedLow}
+      high={sterilizedHigh}
+      {...rest}
+    />
+  );
+};
+
+export default Meter;
--- src/components/index.ts
+++ src/components/index.ts
@@ -13,4 +13,5 @@ export { default as ListGroup } from './ListGroup';
 export { default as Image } from './Image';
 export { default as Details } from './Details';
 export { default as Form } from './Form';
+export { default as Meter } from './Meter';
 export { default as Jumbotron } from './Jumbotron';
