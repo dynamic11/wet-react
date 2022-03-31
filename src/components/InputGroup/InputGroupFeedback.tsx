d1db7ff84b5f6a4f66aa0817aa9a974ee27cd636
Mayowa Awosiyan <mayowaa4@gmail.com>
2022-03-31 00:12:34 -0400

    Validation Story

---
+++ src/components/InputGroup/InputGroupFeedback.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import '../../style.css';
+
+type typeType = 'valid' | 'invalid';
+
+export interface InputGroupFeedbackProps {
+  /** Content of the Feedback */
+  children?: React.ReactNode;
+  /** Display feedback as a tooltip. */
+  tooltip?: boolean;
+  /** Specify whether the feedback is for valid or invalid fields */
+  type?: typeType;
+  /** Additional custom classNames */
+  className?: string;
+}
+
+const InputGroupFeedback = ({
+  children,
+  tooltip,
+  type = 'valid',
+  className = '',
+  ...rest
+}: InputGroupFeedbackProps) => (
+  <Form.Control.Feedback
+    tooltip={tooltip}
+    type={type}
+    className={className}
+    {...rest}
+  >
+    {children}
+  </Form.Control.Feedback>
+);
+
+InputGroupFeedback.displayName = 'InputGroup.Feedback';
+
+export default InputGroupFeedback;
--- src/components/InputGroup/index.ts
+++ src/components/InputGroup/index.ts
@@ -1,15 +1,18 @@
 import OriginInputGroup from './InputGroup';
 import FormControl from './FormControl';
+import InputGroupFeedback from './InputGroupFeedback';
 import InputGroupText from './InputGroupText';
 
 export type InputGroupProps = typeof OriginInputGroup & {
   Control: typeof FormControl;
   Text: typeof InputGroupText;
+  Feedback: typeof InputGroupFeedback;
 };
 
 const InputGroup = OriginInputGroup as InputGroupProps;
 
 InputGroup.Control = FormControl;
 InputGroup.Text = InputGroupText;
+InputGroup.Feedback = InputGroupFeedback;
 
 export default InputGroup;
