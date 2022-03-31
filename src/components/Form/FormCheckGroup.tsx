2b17c997492cc70dd9f892f5ab81794e38c1800b
alphaoreo <61680329+alphaoreo@users.noreply.github.com>
2022-03-31 11:39:48 -0400

    Removed Commented code (#88)

--- src/components/Form/FormCheckGroup.tsx
+++ src/components/Form/FormCheckGroup.tsx
@@ -2,9 +2,6 @@ import React, { useContext } from 'react';
 import FormLabel from './FormLabel';
 import FormGroupContext from './FormGroupContext';
 
-/** Types */
-// type typeType = 'checkbox' | 'radio' | 'switch' | undefined;
-
 export interface FormCheckGroupProps extends React.HTMLAttributes<HTMLElement> {
   /** Content of form label */
   children?: React.ReactNode;
