aaee14a03e2e0921db5e034a83f31c615ea3ab09
Mayowa Awosiyan <mayowaa4@gmail.com>
2022-03-31 11:36:16 -0400

    Added some tests

---
+++ src/components/InputGroup/InputGroup.test.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import InputGroup from '@components/InputGroup';
+
+describe('InputGroup tests', () => {
+  describe('InputGroup size tests', () => {
+    test('Large InputGroup test', () => {
+      render(
+        <InputGroup size="lg">
+          <InputGroup.Text id="test target">Username:</InputGroup.Text>
+          <InputGroup.Control
+            type="text"
+            isRequired
+            placeholder="Gonna be tested"
+            aria-describedby="test target"
+          />
+        </InputGroup>
+      );
+      expect(screen.getByRole('textbox').parentElement).toHaveClass(
+        'input-group-lg'
+      );
+    });
+    test('Small InputGroup test', () => {
+      render(
+        <InputGroup size="sm">
+          <InputGroup.Text id="test target">Username:</InputGroup.Text>
+          <InputGroup.Control
+            type="text"
+            isRequired
+            placeholder="Gonna be tested"
+            aria-describedby="test target"
+          />
+        </InputGroup>
+      );
+      expect(screen.getByRole('textbox').parentElement).toHaveClass(
+        'input-group-sm'
+      );
+    });
+    test('Default size InputGroup test', () => {
+      render(
+        <InputGroup>
+          <InputGroup.Text id="test target">Username:</InputGroup.Text>
+          <InputGroup.Control
+            type="text"
+            isRequired
+            placeholder="Gonna be tested"
+            aria-describedby="test target"
+          />
+        </InputGroup>
+      );
+      expect(screen.getByRole('textbox').parentElement).not.toHaveClass(
+        'input-group-lg'
+      );
+      expect(screen.getByRole('textbox').parentElement).not.toHaveClass(
+        'input-group-sm'
+      );
+    });
+  });
+  describe('InputGroup Validation Tests', () => {
+    test('Has no validation test', () => {
+      render(
+        <InputGroup>
+          <InputGroup.Text id="test target">Username:</InputGroup.Text>
+          <InputGroup.Control
+            type="text"
+            isRequired
+            placeholder="Gonna be tested"
+            aria-describedby="test target"
+          />
+        </InputGroup>
+      );
+      expect(screen.getByRole('textbox').parentElement).not.toHaveClass(
+        'has-validation'
+      );
+    });
+    test('Has validation test', () => {
+      render(
+        <InputGroup hasValidation>
+          <InputGroup.Text id="test target">Username:</InputGroup.Text>
+          <InputGroup.Control
+            type="text"
+            isInvalid
+            placeholder="Gonna be tested"
+            aria-describedby="test target"
+          />
+          <InputGroup.Feedback tooltip type="invalid">
+            Validation
+          </InputGroup.Feedback>
+        </InputGroup>
+      );
+      expect(screen.getByRole('textbox').parentElement).toHaveClass(
+        'has-validation'
+      );
+    });
+  });
+});
