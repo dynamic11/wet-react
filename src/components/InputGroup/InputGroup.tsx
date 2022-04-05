2ea02a6245558a79c237d69a451e49509be725e9
Mayowa Awosiyan <mayowaa4@gmail.com>
2022-04-05 09:22:17 -0400

    Removed Validation props

--- src/components/InputGroup/InputGroup.tsx
+++ src/components/InputGroup/InputGroup.tsx
@@ -12,31 +12,20 @@ export interface InputGroupProps
   children?: React.ReactNode;
   /** Control the size of buttons and form elements from the top-level. */
   size?: sizeType;
-  /** Handles the input's rounded corners when using form validation. Use this when your input group contains both an input and feedback element. */
-  hasValidation?: boolean;
   /** Additional custom classNames */
   className?: string;
 }
 
 const InputGroup = ({
   children,
-  hasValidation,
   size,
   className = '',
   ...rest
-}: InputGroupProps) => {
-  const validationClass = hasValidation === true ? 'has-feedback' : '';
-  return (
-    <InputGroupRB
-      size={size}
-      className={`${validationClass} ${className}`}
-      hasValidation={hasValidation}
-      {...rest}
-    >
-      {children}
-    </InputGroupRB>
-  );
-};
+}: InputGroupProps) => (
+  <InputGroupRB size={size} className={className} {...rest}>
+    {children}
+  </InputGroupRB>
+);
 
 InputGroup.displayName = 'InputGroup';
 
