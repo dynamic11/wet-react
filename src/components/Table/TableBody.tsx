f447c243f4a506b3b5c28ae872a65cb5a6481bb7
Ali Nouri <dynamic11@gmail.com>
2022-04-22 11:13:10 -0400

    [Fix] fix documentation for Table component

---
+++ src/components/Table/TableBody.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../style.css';
+
+export interface TableBodyProps
+  extends React.HTMLAttributes<HTMLTableSectionElement> {
+  /** The content of the table. Each child should be a column title in a <th> tag */
+  children?: React.ReactNode;
+  /** Set additional classnames for the table */
+  className?: string;
+}
+
+const TableBody = ({ children, className, ...rest }: TableBodyProps) => (
+  <tbody className={className} {...rest}>
+    {children}
+  </tbody>
+);
+
+TableBody.displayName = 'Table.Header';
+
+export default TableBody;
